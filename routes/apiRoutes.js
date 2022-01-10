cd2ca48d12d830ef287745b41d28d8251fe2acb0
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-10 08:49:49 -0600

    got delete function working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,13 +3,10 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 
-
 //shows notes from db
 router.get('/notes', function (req, res) {
     notesData = fs.readFileSync('./db/db.json', 'utf-8'),
     res.send(notesData);
-    console.log("baarg")
-    console.log(typeof notesData)
     }
   );
 
@@ -17,12 +14,25 @@ router.post('/notes', (req, res) => {
     notesData = fs.readFileSync("./db/db.json", "utf8");  
     newNotesData = JSON.parse(notesData);
     req.body.id = newNotesData.length;
-    newNotesData.push(req.body); // req.body - user input
+    newNotesData.push(req.body); // req.body = user input
     newNotesData = JSON.stringify(newNotesData);
     fs.writeFile("./db/db.json", newNotesData, "utf8", function(err) {
       if (err) throw err;
       res.json(notesData);
     });
-    });
+  });
+
+router.delete('/notes/:id', function (req, res) {
+     notesData = fs.readFileSync('./db/db.json', 'utf-8');
+     newNotesData = JSON.parse(notesData);
+     console.log(newNotesData)
+  var filterNote = newNotesData.filter(note => note.id != req.params.id)
+    console.log(filterNote) 
+    filterNote = JSON.stringify(filterNote)
+    fs.writeFile("./db/db.json", filterNote, "utf8", function(err) {
+      if (err) throw err;
+      res.json(notesData);
+      })
+    })
 
   module.exports = router;
